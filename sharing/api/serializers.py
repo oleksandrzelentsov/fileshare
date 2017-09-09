3ce6bed24b7df39811bc0a55b9b8d3c0794d9952
Oleksandr Zelentsov <saze1997@gmail.com>
2017-09-09 16:14:52 +0200

    added permissions etc, completed

--- sharing/api/serializers.py
+++ sharing/api/serializers.py
@@ -1,26 +1,41 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from sharing.models import ShareableFile
 
 
 class ShareableFileListSerializer(ModelSerializer):
+    url = SerializerMethodField('get_raw_url')
+
+    def get_raw_url(self, obj):
+        return obj.get_raw_url()
+
     class Meta:
         model = ShareableFile
         fields = [
-            'id',
             'name',
-            'get_raw_url',
+            'hash',
+            'url',
             'public',
         ]
 
 
 class ShareableFileDetailSerializer(ModelSerializer):
+    url = SerializerMethodField('get_raw_url')
+
+    def get_raw_url(self, obj):
+        return obj.get_raw_url()
+
     class Meta:
         model = ShareableFile
+        read_only_fields = (
+            'hash',
+            'url',
+        )
         fields = [
-            'id',
             'name',
-            'get_raw_url',
+            'hash',
+            'url',
             'public',
         ]
 
@@ -28,7 +43,9 @@ class ShareableFileDetailSerializer(ModelSerializer):
 class ShareableFileCreateSerializer(ModelSerializer):
     class Meta:
         model = ShareableFile
-        read_only_fields = ('user', )
+        read_only_fields = (
+            'user',
+        )
         fields = [
             'file',
             'user',
--- sharing/api/urls.py
+++ sharing/api/urls.py
@@ -7,9 +7,9 @@ from sharing.api.views import LoginView, LogoutView, ShareableFileListView, Shar
 urlpatterns = [
     url(r'^files/$', ShareableFileListView.as_view(), name='files-list'),
     url(r'^files/create/$', ShareableFileCreateView.as_view(), name='file-create'),
-    url(r'^files/(?P<pk>\d+)/$', ShareableFileDetailView.as_view(), name='file-retrieve'),
-    url(r'^files/(?P<pk>\d+)/update/$', ShareableFileUpdateView.as_view(), name='file-update'),
-    url(r'^files/(?P<pk>\d+)/delete/$', ShareableFileDeleteView.as_view(), name='file-delete'),
+    url(r'^files/(?P<hash>[\w\d]{40})/$', ShareableFileDetailView.as_view(), name='file-retrieve'),
+    url(r'^files/(?P<hash>[\w\d]{40})/update/$', ShareableFileUpdateView.as_view(), name='file-update'),
+    url(r'^files/(?P<hash>[\w\d]{40})/delete/$', ShareableFileDeleteView.as_view(), name='file-delete'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='login'),
 ]
