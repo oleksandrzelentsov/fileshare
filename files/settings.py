fe18fb25539b64e9a9276cd3d2816d36b5220ad0
Oleksandr Zelentsov <saze1997@gmail.com>
2017-09-12 23:03:11 +0200

    settings for heroku

---
+++ files/settings.py
@@ -0,0 +1,13 @@
+from files.base_settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fileshare',
+        'USER': 'oleksandr',
+        'PASSWORD': '383R5cix',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
