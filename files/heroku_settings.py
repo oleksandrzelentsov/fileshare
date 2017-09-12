de12607ca1c0e7c9339d2b10ead85d69afd28300
Oleksandr Zelentsov <saze1997@gmail.com>
2017-09-12 23:53:27 +0200

    db

--- files/heroku_settings.py
+++ files/heroku_settings.py
@@ -1,13 +1,4 @@
 from files.base_settings import *
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('FILESHARE_DB_NAME'),
-        'USER': os.environ.get('FILESHARE_DB_USER'),
-        'PASSWORD': os.environ.get('FILESHARE_DB_PASSWORD'),
-        'HOST': os.environ.get('FILESHARE_DB_HOST'),
-        'PORT': os.environ.get('FILESHARE_DB_PORT'),
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ["DATABASE_URL"])}
